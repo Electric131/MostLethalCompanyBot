43a36bd2986b9c9651a30fffce03c198b1a8e774
Electric131 <39623923+Electric131@users.noreply.github.com>
2024-02-04 22:18:23 -0600

    Added tag and WIP thread auto-replying

--- src/Events/messageCreate.ts
+++ src/Events/messageCreate.ts
@@ -8,6 +8,15 @@ export const event: Event = {
         if (message.channel.id == config().updates_channel) {
             if (message.crosspostable && message.content.match(config().updates_regex)) {
                 message.crosspost();
+                return;
+            }
+        }
+        // Message was posted in a thread, and thread is the support thread, and message was sent by OP
+        if (message.thread && message.thread.id == config().support_thread_id && message.thread.ownerId == message.author.id) {
+            console.log(message.content);
+            // Message contains either ("pirated" or "cracked") AND contains either ("game" or "version")
+            if ((message.content.includes("pirated") || message.content.includes("cracked")) && (message.content.includes("game") || message.content.includes("version"))) {
+                console.log("flagged");
             }
         }
     }
