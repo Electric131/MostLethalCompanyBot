7ef9665dacff281588b6857d06677c7532e592b4
Electric131 <39623923+Electric131@users.noreply.github.com>
2024-03-01 13:55:25 -0600

    Added logs for which urls were caught

--- src/Util/urlCheck.ts
+++ src/Util/urlCheck.ts
@@ -1,4 +1,4 @@
-import { Message } from "discord.js";
+import { Message, TextChannel } from "discord.js";
 import { config } from "./data";
 
 // Regex string to match parts of a url, used to verify if the url is valid
@@ -9,19 +9,27 @@ export async function handleMessage(message: Message): Promise<boolean> {
     let matches = message.content.matchAll(urlRegex);
 
     let validMessage = true;
+    let failed = [];
     for (let match of matches) {
         let result = checkURL(match);
         if (!result) {
             validMessage = false;
-            break;
+            failed.push(match);
         }
     }
 
+    let prettyFailed = failed.map(val => val[0]); // Get first "match" in each url match. (This will be the entire matched string)
+
     // All URLs in message are valid
     if (!validMessage) {
-        console.log(`Message sent by ${message.author.username} (${message.author.id}) was deleted for urls.`);
-        await message.author.send(`Your message (In https://discord.com/channels/${message.guildId}/${message.channelId}) contains disallowed urls. Please only send urls relating to the server. (ie. thunderstore, youtube, ect.) If you believe this is a mistake, please contact Clark.`);
         await message.delete();
+        console.log(`Message sent by ${message.author.username} (${message.author.id}) was deleted for urls. (${prettyFailed.join(", ")})`);
+        message.client.channels.fetch(config()["log_channel"]).then((channel) => {
+            let embed = config()["whitelist_fail_embed"];
+            embed.embeds[0].description += prettyFailed.join("\n");
+            (channel as TextChannel).send(embed);
+        })
+        await message.author.send(`Your message (In https://discord.com/channels/${message.guildId}/${message.channelId}) contains disallowed urls. Please only send urls relating to the server. (ie. thunderstore, youtube, ect.) If you believe this is a mistake, please contact Clark.`);
         return false;
     }
     return true;
